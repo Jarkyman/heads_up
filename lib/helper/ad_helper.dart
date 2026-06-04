e067e3e753f4eabebf9f2a2fe56952f454274a79
Jackie Jensen <Jackiehartvig@hotmail.com>
2026-06-04 21:24:55 +0200

    refactor: simplify ad unit ID retrieval logic and remove unnecessary code

--- lib/helper/ad_helper.dart
+++ lib/helper/ad_helper.dart
@@ -1,95 +1,64 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:heads_up/controllers/settings_controller.dart';
 
 class AdHelper {
   static String get bannerAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
-      if (Platform.isAndroid) {
-        return '';
-      } else if (Platform.isIOS) {
-        return '';
-      } else {
-        throw UnsupportedError('Unsupported platform');
-      }
-    } else {
-      return 'No ad banner';
-    }
-  }
+    if (Get.find<SettingsController>().isUnlockAll) return '';
 
-  static String get interstitialAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
-      if (Platform.isAndroid) {
-        return 'ca-app-pub-9894760850635221/7880130767';
-      } else if (Platform.isIOS) {
-        return 'ca-app-pub-9894760850635221/3551941316';
-      } else {
-        throw UnsupportedError("Unsupported platform");
-      }
-    } else {
-      return "no ad interstitialAd";
-    }
-  }
-
-  static String get rewardedAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
-      if (Platform.isAndroid) {
-        return 'ca-app-pub-9894760850635221/4870824043';
-      } else if (Platform.isIOS) {
-        return 'ca-app-pub-9894760850635221/4268188814';
-      } else {
-        throw UnsupportedError("Unsupported platform");
-      }
-    } else {
-      return "no ad rewardedAd";
-    }
-  }
-}
-
-// TEST
-/*
-class AdHelper {
-  static String get bannerAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
+    if (kDebugMode) {
       if (Platform.isAndroid) {
         return 'ca-app-pub-3940256099942544/6300978111';
       } else if (Platform.isIOS) {
         return 'ca-app-pub-3940256099942544/2934735716';
-      } else {
-        throw UnsupportedError('Unsupported platform');
       }
-    } else {
-      return 'No ad banner';
     }
+
+    if (Platform.isAndroid) {
+      return '';
+    } else if (Platform.isIOS) {
+      return '';
+    }
+    return '';
   }
 
   static String get interstitialAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
+    if (Get.find<SettingsController>().isUnlockAll) return '';
+
+    if (kDebugMode) {
       if (Platform.isAndroid) {
         return "ca-app-pub-3940256099942544/1033173712";
       } else if (Platform.isIOS) {
         return "ca-app-pub-3940256099942544/4411468910";
-      } else {
-        throw UnsupportedError("Unsupported platform");
       }
-    } else {
-      return "no ad interstitialAd";
     }
+
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-9894760850635221/7880130767';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-9894760850635221/3551941316';
+    }
+    return '';
   }
 
   static String get rewardedAdUnitId {
-    if (!Get.find<SettingsController>().isUnlockAll) {
+    if (Get.find<SettingsController>().isUnlockAll) return '';
+
+    if (kDebugMode) {
       if (Platform.isAndroid) {
         return "ca-app-pub-3940256099942544/5224354917";
       } else if (Platform.isIOS) {
         return "ca-app-pub-3940256099942544/1712485313";
-      } else {
-        throw UnsupportedError("Unsupported platform");
       }
-    } else {
-      return "no ad RewardedAd";
     }
+
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-9894760850635221/4870824043';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-9894760850635221/4268188814';
+    }
+    return '';
   }
 }
-*/
